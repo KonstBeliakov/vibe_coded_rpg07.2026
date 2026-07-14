cb04fd6bfe446e58a9375f428f205ebfb1b52169
KonstBeliakov <konstbeliakov@gmail.com>
2026-07-15 02:54:30 +0300

    Исправление ошибки BIOME_DESERT is not defined - добавлены недостающие константы биомов и их конфигурации

--- js/biomes.js
+++ js/biomes.js
@@ -6,6 +6,10 @@ const BIOME_MOSSY = 1;
 const BIOME_WEB = 2;
 const BIOME_LAVA = 3;
 const BIOME_ICE = 4;
+const BIOME_DESERT = 5;
+const BIOME_SWAMP = 6;
+const BIOME_MAGIC = 7;
+const BIOME_CRYSTAL = 8;
 
 // Biome configuration
 const BIOME_CONFIG = {
@@ -38,6 +42,30 @@ const BIOME_CONFIG = {
         floorColor: '#1a2a3a',
         wallColor: '#3a4a5a',
         description: 'Холодный, со льдом'
+    },
+    [BIOME_DESERT]: {
+        name: 'Пустынный',
+        floorColor: '#3a3a1a',
+        wallColor: '#5a5a3a',
+        description: 'Жаркая пустыня'
+    },
+    [BIOME_SWAMP]: {
+        name: 'Болотный',
+        floorColor: '#1a3a2a',
+        wallColor: '#3a4a3a',
+        description: 'Топкое болото'
+    },
+    [BIOME_MAGIC]: {
+        name: 'Магический',
+        floorColor: '#2a1a3a',
+        wallColor: '#4a3a5a',
+        description: 'Наполнен магией'
+    },
+    [BIOME_CRYSTAL]: {
+        name: 'Кристальный',
+        floorColor: '#1a2a3a',
+        wallColor: '#3a5a5a',
+        description: 'Сверкающий кристаллами'
     }
 };
 
@@ -50,14 +78,20 @@ const BIOME_CONFIG = {
  */
 function getBiomeAt(perlin, tileX, tileY) {
     const biomeNoise = perlin.octaveNoise(tileX * 0.03, tileY * 0.03, 2, 0.5);
-    if (biomeNoise > 0.3 && biomeNoise < 0.45) {
+    if (biomeNoise > 0.3 && biomeNoise < 0.4) {
         return BIOME_MOSSY;
-    } else if (biomeNoise >= 0.45 && biomeNoise < 0.55) {
+    } else if (biomeNoise >= 0.4 && biomeNoise < 0.5) {
         return BIOME_WEB;
-    } else if (biomeNoise >= 0.55 && biomeNoise < 0.65) {
+    } else if (biomeNoise >= 0.5 && biomeNoise < 0.6) {
         return BIOME_LAVA;
-    } else if (biomeNoise >= 0.65) {
+    } else if (biomeNoise >= 0.6 && biomeNoise < 0.7) {
         return BIOME_ICE;
+    } else if (biomeNoise >= 0.7 && biomeNoise < 0.8) {
+        return BIOME_DESERT;
+    } else if (biomeNoise >= 0.8 && biomeNoise < 0.9) {
+        return BIOME_SWAMP;
+    } else if (biomeNoise >= 0.9) {
+        return BIOME_MAGIC;
     }
     return BIOME_NORMAL;
 }
